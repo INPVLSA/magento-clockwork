b5701c45e584935872ae89fb25e0f94cd75eb486
Uladzislau Marudzenka <vmarudenko@gomage.com>
2025-03-21 23:10:31 +0400

    Fix profiler names

--- view/frontend/web/js/clockwork-web.js
+++ view/frontend/web/js/clockwork-web.js
@@ -21,10 +21,43 @@
 
     function initialize() {
         for (const dbRecord of document.querySelectorAll('tr .ui-icon[data-name="database"]')) {
-            dbRecord.closest('tr').setAttribute('data-row-type', 'db');
+            const row = dbRecord.closest('tr');
+
+            if (row.getAttribute('data-row-type') === 'db') {
+                continue;
+            }
+            row.setAttribute('data-row-type', 'db');
         }
-        document.querySelector('a.header-item[title="Database"]').addEventListener('click', onToggleDbView);
+        const dbSwitchButton = document.querySelector('a.header-item[title="Database"]');
+
+        if (dbSwitchButton) {
+            dbSwitchButton.addEventListener('click', onToggleDbView);
+        }
+
+        document.querySelectorAll('a.details-header-tab').forEach((tab) => {
+            tab.addEventListener('click', initWithWait)
+
+            if (tab.getAttribute('data-tab-initialized') === 'true') {
+                return;
+            }
+
+            tab.setAttribute('data-tab-initialized', 'true');
+        });
+
+        document.querySelectorAll('#requests tr').forEach((requestRow) => {
+            requestRow.addEventListener('click', initWithWait)
+
+            if (requestRow.getAttribute('data-request-initialized') === 'true') {
+                return;
+            }
+            requestRow.setAttribute('data-request-initialized', 'true');
+        });
     }
 
-    setTimeout(initialize, 1000);
+    function initWithWait() {
+        setTimeout(initialize, 1000);
+    }
+
+    // Main component not dispatching any events, so need to wait a bit
+    initWithWait();
 })();
